cfa722442e1617b832af8d0b617be69d6623870e
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2011-12-04 11:17:45 +0100

    Implements pages target from makefile + make_pages bash script in batch

---
+++ win_tools/pages.cmd
@@ -0,0 +1,15 @@
+REM Try to implement "pages" target AND make_pages script
+REM Assuming our dear fucking windows user managed to install
+REM perl correctly and we can access it trhough the perl.exe cmd
+
+IN_DIR=$1
+OUTPUT_DIR=$2
+TEMPLATE=$3
+
+mkdir pages
+for /r src %%G do (
+    echo "Converting..."
+    type template/begin > pages/%%~nG.mkd
+    perl.exe bin/Markdown.pl $i >> pages/%%~nG.mkd
+    type template/end >> pages/%%~nG.mkd
+)
